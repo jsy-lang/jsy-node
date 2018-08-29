4053966391440224dd8c14dde0ed4354ad88b73d
Shane Holloway <shane.holloway@ieee.org>
2018-08-29 14:53:51 -0600

    Added a --version option to jsy-node

--- jsy-node.js
+++ jsy-node.js
@@ -3,4 +3,10 @@ const jsy_module = require.resolve('jsy-node/jsy.js')
 const [cmd, _, ...args] = process.argv.slice()
 const argv = ['--require', jsy_module].concat(args)
 
-require('kexec')(cmd, argv)
+if (~ args.indexOf('--version')) {
+  const pkg = require('./package.json')
+  console.log(`JSY: ${pkg.version}  NodeJS: ${process.version}`)
+  return 0
+} else {
+  require('kexec')(cmd, argv)
+}
