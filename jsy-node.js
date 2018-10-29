0e7dd8ab8e31a14e11f4486fbb049240e4b5bc44
Shane Holloway <shane.holloway@ieee.org>
2018-10-29 16:05:14 -0600

    Updated dependencies and removed kexec compiled dependency

--- jsy-node.js
+++ jsy-node.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 const jsy_module = require.resolve('jsy-node/jsy.js')
+const { spawnSync } = require('child_process')
 const [cmd, _, ...args] = process.argv.slice()
 const argv = ['--require', jsy_module].concat(args)
 
 if (~ args.indexOf('--version')) {
   const pkg = require('./package.json')
   console.log(`JSY: ${pkg.version}  NodeJS: ${process.version}`)
-  return 0
+  process.exit(0)
 } else {
-  require('kexec')(cmd, argv)
+  const res = spawnSync(cmd, argv, { shell: true, stdio: 'inherit' })
+  process.exit(res.status)
 }
