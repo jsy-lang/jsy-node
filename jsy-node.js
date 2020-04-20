1a394f7704dc5f338f268a491d0d306b82d30f4e
Shane Holloway <shane.holloway@ieee.org>
2020-04-20 12:16:58 -0600

    v1.6.2

--- jsy-node.js
+++ jsy-node.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 const jsy_module = require.resolve('jsy-node/jsy.js')
 const { spawnSync } = require('child_process')
-const [cmd, _, ...args] = process.argv.slice()
-const argv = ['--require', jsy_module].concat(args)
+const args = process.argv.slice(2)
 
-if (~ args.indexOf('--version')) {
+if (0 === args.length || ~ args.indexOf('--version')) {
   const pkg = require('./package.json')
-  console.log(`JSY: ${pkg.version}  NodeJS: ${process.version}`)
+  console.log(`jsy-transpile: ${pkg.devDependencies['jsy-transpile']}  jsy-node: ${pkg.version}  NodeJS: ${process.version} `)
   process.exit(0)
 } else {
-  const res = spawnSync(cmd, argv, { shell: true, stdio: 'inherit' })
+  const argv = ['--require', jsy_module].concat(args)
+  const res = spawnSync(process.execPath, argv, { shell: true, stdio: 'inherit' })
   process.exit(res.status)
 }
--- jsy.js
+++ jsy.js
@@ -1,4 +1,4 @@
-require('source-map-support').install()
+try { require('source-map-support/register.js') } catch (err) {}
 const pirates = require('pirates')
 
 const jsy_pirate_hook = require('./cjs/jsy_pirate_hook.js')
